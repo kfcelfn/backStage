f7a94572f4cd367a6dbcf973bda0bcd2783a2dae
kfcelfn <595330682@qq.com>
2020-05-18 16:09:25 +0800

    '上传功能优化'

--- src/pages/home/form.jsx
+++ src/pages/home/form.jsx
@@ -3,30 +3,23 @@ import { Form, Input, Button, message, Upload, Icon } from 'antd';
 import { connect } from 'react-redux';
 import { addUser } from '@/actions/home'
 
-export default @connect( state  => { 
-  return {
-    
-  } 
-},{
+export default @connect(state  => {return {}},{
   addUser 
 })
 
 @Form.create()
 class extends Component {
-  state = {
-    imgUrl: ''
-  }
 
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        values.hospital = `/${this.state.imgUrl}`
-        console.log(values)
+        const imgs = `/${values.hospital[0].name}`
+        values.hospital = imgs
         this.props.addUser(values)
-          .then( res => {
+          .then( () => {
             message.success('添加成功');
-            // this.props.history.push('/home/table')
+            this.props.history.push('/home/table')
           })
       }
     })
@@ -35,34 +28,21 @@ class extends Component {
   handleReset = () => {
     this.props.form.resetFields();
   }
+
+  normFile = e => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  };
   
   render() {
     const { getFieldDecorator } = this.props.form;
-    const _this = this
-
-    const props = {
-      name: 'file',
-      action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-      headers: {
-        authorization: 'authorization-text',
-      },
-      onChange(info) {
-        if (info.file.status === 'done') {
-          message.success(`上传成功`);
-          console.log(info)
-          _this.setState({imgUrl: info.file.name})
-
-        } else if (info.file.status === 'error') {
-          message.error(`${info.file.name} file upload failed.`);
-        }
-      },
-    }
 
     return (
       <div className='home_form'>
         <h1>Basic Form</h1>
         <h2>Basic form elements</h2>
-        <p>Name</p>
         <Form onSubmit={this.handleSubmit}>
           <Form.Item label="Name">
             {getFieldDecorator('name', {
@@ -95,11 +75,16 @@ class extends Component {
             })(<Input />)}
           </Form.Item>
           <Form.Item label="Upload">
-            <Upload {...props}>
-              <Button>
-                <Icon type="upload" /> Click to upload
-              </Button>
-            </Upload>
+            {getFieldDecorator('hospital', {
+              valuePropName: 'fileList',
+              getValueFromEvent: this.normFile,
+            })(
+              <Upload name="logo" action="https://www.mocky.io/v2/5cc8019d300000980a055e76" listType="picture">
+                <Button>
+                  <Icon type="upload" /> Click to upload
+                </Button>
+              </Upload>,
+            )}
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit">
